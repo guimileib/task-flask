c327f18b26bb5fd62e12868364b10b763d3f73e5
Guilherme Mileib <guilhermemileib@gmail.com>
2024-06-10 16:56:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from models.task import Task
 app = Flask(__name__)
 
@@ -6,12 +6,16 @@ app = Flask(__name__)
 # Create, Read, Update and Delete
 
 tasks = []
-
+task_id_control = 1
 @app.route('/tasks', methods=['POST'])
 def create_task():
+    global task_id_control
     data = request.get_json()
-    print(data)
-    return 'Test'
+    new_task = Task(id=task_id_control, title=data.get("title"), description=data.get("description", ""))
+    task_id_control += 1 # Soma 1 no identificador
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message":  "Nova tarefa criada com sucesso"}) # Retormo com dicionário
 
 if __name__ == "__main__":
     app.run(debug=True) 
